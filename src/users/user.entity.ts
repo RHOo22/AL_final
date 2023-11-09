2c8e38fe0e4e64bf426d459f215ae1ccee38b953
theolame <theolaminie10@gmail.com>
2023-11-09 11:18:31 +0100

    TypeORM

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,7 +2,7 @@ export class User{
     lastname?: string;
     firstname?: string;
     id!: number;
-    age?: number
+    age: number
     constructor(id:number, ln:string, fn:string, age:number){
         this.id = id;
         this.lastname = ln;
