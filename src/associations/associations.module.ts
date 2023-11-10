5b613ae79c6bd1059d4e0a8f867d91c6b891b609
theolame <theolaminie10@gmail.com>
2023-11-10 11:21:44 +0100

    bug import

--- src/associations/associations.module.ts
+++ src/associations/associations.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AssociationsController } from './associations.controller';
 import { AssociationsService } from './associations.service';
-import { UsersModule } from 'src/users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Association } from './association.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Association])],
   controllers: [AssociationsController],
-  providers: [AssociationsService],
-  imports: [UsersModule]
+  providers: [AssociationsService]
 })
 export class AssociationsModule {}
