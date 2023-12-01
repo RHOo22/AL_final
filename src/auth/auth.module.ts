de79bbc118b1c9436ca7c3c936bea54d0480e830
Rault Antoine <antoine.rault.1@etudiant.univ-rennes1.fr>
2023-12-01 12:13:16 +0100

    début auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,13 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './auth.constants';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, 
+      PassportModule, 
+      JwtModule.register({secret: jwtConstants.secret, signOptions: { expiresIn: '60s' }})],
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController]
 })
