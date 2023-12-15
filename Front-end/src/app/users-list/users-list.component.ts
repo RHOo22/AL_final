8b9cb5ee907cbe0eb9d59a4c97e027f46233cc36
RHOo22 <2711a.rault@gmail.com>
2023-12-15 09:26:23 +0100

    start

---
+++ Front-end/src/app/users-list/users-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-users-list',
+  templateUrl: './users-list.component.html',
+  styleUrl: './users-list.component.css'
+})
+export class UsersListComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  displayedColumns: string[] = ['id', 'lastname', 'firstname', 'age'];
+  dataSource = users;
+  
+
+}
+
+export class User {
+  constructor(
+    public id: number,
+    public password: string,
+    public lastname: string,
+    public firstname: string,
+    public age: number
+  ) {}
+}
+const users: User[] = [
+  new User(0, 'mdp1', 'Doe', 'John', 23),
+  new User(1, 'mdp2', 'Doe', 'Jane', 32)
+]
