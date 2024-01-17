54c9389527ef6b5882eb79f714aa177108752758
RHOo22 <2711a.rault@gmail.com>
2024-01-17 15:46:28 +0100

    first commt

--- Back-end/dist/main.js
+++ Back-end/dist/main.js
@@ -6,7 +6,7 @@ const app_module_1 = require("./app.module");
 const helmet_1 = require("helmet");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors({ 'origin': 'http://localhost:4200' });
+    app.enableCors({ 'origin': 'http://localhost:8000' });
     app.use((0, helmet_1.default)());
     const config = new swagger_1.DocumentBuilder()
         .setTitle('Gestion des Associations')
